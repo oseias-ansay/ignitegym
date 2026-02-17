c088587f58d0b60db6acd51ab6c146e4bd130dfa
Oseias <oseias.ansay@gmail.com>
2026-02-17 11:24:12 -0300

    Interface Home

---
+++ src/compenents/HomeHeader.tsx
@@ -0,0 +1,23 @@
+import { Heading, HStack, Icon, Text, VStack } from '@gluestack-ui/themed'
+import { UserPhoto } from './UserPhoto'
+import { LogOut } from 'lucide-react-native'
+
+export function HomeHeader() {
+  return (
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
+      <UserPhoto source={{ uri: 'https://github.com/oseias-ansay.png'}} w='$16' h='$16' alt='Imagem do usuário' />
+
+      <VStack flex={1}>
+        <Text color="$gray100" fontSize="$sm">
+          Olá
+        </Text>
+        <Heading color="$gray100" fontSize="$md">
+          Oséias Ansay
+        </Heading>
+      </VStack>
+
+      <Icon as={LogOut} color='$gray200' size='xl' />
+
+    </HStack>
+  )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,61 @@
-import {Center, ScrollView, Text, VStack} from '@gluestack-ui/themed'
+import { ExerciseCard } from '../compenents/ExerciseCard'
+import { Group } from '../compenents/Group'
+import { HomeHeader } from '../compenents/HomeHeader'
+import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
-export function Home() {        
-    return (    
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <VStack flex={1} bg="$gray700" alignItems="center" justifyContent="center">
-                <Center>    
-                    <Text color="$gray100" fontSize="$xl">Home</Text>
-                </Center>
-            </VStack>
-        </ScrollView>
-    )
+export function Home() {
+  const [exercises, setExercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Remada unilateral',
+    'Levantamento terra',
+    'Levantamento terra',
+    'Levantamento terra',
+    'Levantamento terra',
+  ])
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groupSelected, setGroupSelected] = useState('costas')
+
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
+
+      <VStack px="$8" flex={1}>
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md">
+            Exercícios
+          </Heading>
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
+    </VStack>
+  )
 }
