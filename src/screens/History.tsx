6e4d597032a8f4e3545818aff2a7c8637eed7769
Oseias <oseias.ansay@gmail.com>
2026-02-17 14:35:46 -0300

    Interfaces History e Profile

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,13 +1,46 @@
-import {Center, ScrollView, Text, VStack} from '@gluestack-ui/themed'
+import { HistoryCard } from '../compenents/HistoryCard'
+import { ScreenHeader } from '../compenents/ScreenHeader'
+import { Heading, Text, VStack } from '@gluestack-ui/themed'
+import { useState } from 'react'
+import { SectionList } from 'react-native'
 
-export function History() {         
-    return (    
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <VStack flex={1} bg="$gray700" alignItems="center" justifyContent="center">
-                <Center>    
-                    <Text color="$gray100" fontSize="$xl">History</Text>
-                </Center>
-            </VStack>
-        </ScrollView>
-    )
+export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '22.07.24',
+      data: ['Puxada frontal', 'Remada unilateral'],
+    },
+    {
+      title: '23.07.24',
+      data: ['Puxada frontal'],
+    },
+  ])
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="$gray200" fontSize="$md" mt="$10" mb="$3">
+            {section.title}
+          </Heading>
+        )}
+        style={{ paddingHorizontal: 32 }}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="$gray200" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer execícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
+  )
 }
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,13 +1,64 @@
-import {Center, ScrollView, Text, VStack} from '@gluestack-ui/themed'
+import { Button } from '../compenents/Button'
+import { Input } from '../compenents/Input'
+import { ScreenHeader } from '../compenents/ScreenHeader'
+import { UserPhoto } from '../compenents/UserPhoto'
+import { Center, Heading, Text, VStack } from '@gluestack-ui/themed'
+import { ScrollView, TouchableOpacity } from 'react-native'
 
-export function Profile() {        
-    return (    
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <VStack flex={1} bg="$gray700" alignItems="center" justifyContent="center">
-                <Center>    
-                    <Text color="$gray100" fontSize="$xl">Profile  </Text>
-                </Center>
-            </VStack>
-        </ScrollView>
-    )
+export function Profile() {
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: 'https://github.com/oseias-ansay.png' }}
+            size="xl"
+            alt="Imagem do usuário"
+          />
+
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input value="oseias.ansay@email.com" bg="$gray600" isReadOnly />
+          </Center>
+
+          <Heading
+            alignSelf="flex-start"
+            fontFamily="$heading"
+            color="$gray200"
+            fontSize="$md"
+            mt="$12"
+            mb="$2"
+          >
+            Alterar senha
+          </Heading>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+            <Input
+              placeholder="Confirme a nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+
+            <Button title="Atualizar" />
+          </Center>
+        </Center>
+      </ScrollView>
+    </VStack>
+  )
 }
